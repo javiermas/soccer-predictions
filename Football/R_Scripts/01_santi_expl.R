a85fec6acce036322d9eb454ba4f18a89ae856a9
Santiago Paz <santiago.l.paz@gmail.com>
2018-04-10 00:39:16 +0200

    Adding feature creation

--- Football/R_Scripts/01_santi_expl.R
+++ Football/R_Scripts/01_santi_expl.R
@@ -1,9 +1,40 @@
+rm(list=ls())
 
-setwd("~/Desktop/soccer-predictions")
-all_data <- read.csv('Football/all_data/combined_data.csv')
+setwd("~/Desktop/soccer-predictions/Football/all_data")
+# all_data <- read.csv('Football/all_data/combined_data.csv')
 
-## Test some stuff
-temp <- subset(all_data, Div == 'D1', select = c('PSA', 'Date'))
+library(plyr)
+
+
+## ----------- Combine Data ------------ ## 
+
+years_for_search_and_extract = c('1718','1617','1516','1415','1314','1213','1112',
+                                '1011','0910','0809','0708','0607','0506','0405',
+                                '0304','0203','0102','0001','9900','9899','9798')
+
+combined.df <- list()
+
+for (i in 1:length(years_for_search_and_extract)){
+  
+    
+  setwd(paste("~/Desktop/soccer-predictions/Football/all_data",years_for_search_and_extract[i],sep='/'))
+  
+    files  <- list.files(pattern = '\\.csv')
+    
+    # read files into a list - are there headers?
+    
+    tables <- lapply(files, read.csv, header = TRUE)
+    
+    # rbind files
+    
+    combined.df[[i]] <- do.call(rbind.fill , tables)
+    combined.df[[i]]$Season <- rep(years_for_search_and_extract[i], nrow(combined.df[[i]]))
+    
+}
+
+all_data <- do.call(rbind.fill, combined.df[])
+
+## ----------- Var list/ Clean ------------- ## 
 
 ## Subset only features from list (TBD)
 
@@ -38,14 +69,52 @@ match_vars <- c('Div',
               'FTR',
               'HTHG',
               'HTAG',
-              'HTR')
+              'HTR',
+              'Season')
 
-## Subset feature list (match stats) with no Nas)
-complete_df <- all_data[!is.na(all_data[feature_list]),]
 
-## Extract and remove duplcated columns
-data <- complete_df[,as.vector(rbind(match_vars, feature_list))]
-data <- data[, !duplicated(t(data))]
+## Extract, order, clean
+data <- all_data[,as.vector(rbind(match_vars, feature_list))]
+data <- data[is.na(data$FTR)==FALSE,]
+data<- data[order(as.Date(data$Date, format="%d/%m/%y")),]
+
+
+## --------------- Features ------------- ##
+
+## Create target  
+target <- ifelse(data$FTR=='H', 1, 0)
+
+
+## Agg goals feature
+
+datalist1 <- list()
+datalist2 <- list()
+for (i in 1:length(years_for_search_and_extract)){
+  
+  sub <- subset(data, Season == years_for_search_and_extract[i])
+  
+  l <- unique(sub$HomeTeam)
+  
+  for (j in 1:length(l)){
+    
+    temp <- subset(sub, HomeTeam == l[j], select = c('Date','HomeTeam','FTHG'))
+    temp$agg_goals.f <- cumsum(temp$FTHG)
+    
+    datalist2[[j]] <- temp
+    
+  }
+
+temp2 <- do.call(rbind, datalist2[])
+sub <- merge(x = sub, y = temp2[,c('Date','HomeTeam','agg_goals.f')], by = c("Date","HomeTeam"), all.x = TRUE)
+datalist1[[i]] <- sub    
+  
+}
+
+
+ 
+
+
+
 
 
 
