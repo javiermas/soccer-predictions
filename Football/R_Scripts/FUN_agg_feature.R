988a2b88f66e1c9d67dd4ff4c5cbef9e5440a00a
Santiago Paz <santiago.l.paz@gmail.com>
2018-04-11 00:34:01 +0200

    Aggregate stat function

---
+++ Football/R_Scripts/FUN_agg_feature.R
@@ -0,0 +1,30 @@
+
+agg_feature <- function(HA, var) {
+
+datalist1 <- list()
+datalist2 <- list()
+
+for (i in 1:length(years_for_search_and_extract)){
+  
+  sub <- subset(data, Season == years_for_search_and_extract[i])
+  
+  l <- unique(sub[,HA])
+  
+  for (j in 1:length(l)){
+    
+    temp <- subset(sub, sub[,HA] == l[j], select = c('Date','Div', 'HomeTeam', HA, var))
+    temp$agg_goals.f <- cumsum(temp[,var])
+    
+    datalist1[[j]] <- temp
+    
+  }
+  
+  temp2 <- do.call(rbind, datalist1[])
+  # sub <- merge(x = sub, y = temp2[,c('Date','HomeTeam','agg_goals.f')], by = c("Date","HomeTeam"), all.x = TRUE)
+  datalist2[[i]] <-temp2    
+  
+}
+
+return(datalist2)
+
+}
