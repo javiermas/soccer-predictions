388bf9f36768c36849689d7f173c9bee06bec96a
Javier Mas <javier.mas@glovoapp.com>
2020-01-12 00:18:20 +0530

    :wrench: Add more columns to pivoting

--- soccerlearn/features/pivot_fixtures_by_team.py
+++ soccerlearn/features/pivot_fixtures_by_team.py
@@ -4,9 +4,11 @@ import pandas as pd
 def pivot_fixtures_by_team(data):
     fixtures = data['fixtures']
     odd_vars = [col for col in fixtures if 'odd' in col]
+    cols_to_keep = ['round_id', 'date', 'scores', 'winner_team_id',
+                    'season_id', 'season_start_year']
     features = pd.melt(
         fixtures,
-        id_vars=['date', 'scores', 'winner_team_id', 'season_id'] + odd_vars,
+        id_vars=cols_to_keep+ odd_vars,
         value_vars=['localteam_id', 'visitorteam_id'],
         value_name='team_id'
     )
