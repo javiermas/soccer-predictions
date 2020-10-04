f45cf5768761ab8515ce07e425e10f94a848deae
Javier Mas <javier.mas@glovoapp.com>
2020-10-04 15:33:52 +0200

    :sparkles: Add logging and call method to Pipeline

--- soccerlearn/features/pipeline.py
+++ soccerlearn/features/pipeline.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import logging
 import pandas as pd
 
 
@@ -6,20 +7,24 @@ class Pipeline:
 
     def __init__(self, transformer_funcs):
         self.transformer_funcs = transformer_funcs
+        self.logger = logging.getLogger()
+
+    def __call__(self, data, *args, **kwargs):
+        return self.transform(data)
 
     def transform(self, data):
         for name, func, kwargs in self.transformer_funcs:
             if name:
-                print(f'Creating feature {name}')
+                self.logger.info(f'Creating feature {name}')
                 data[name] = func(data, **kwargs)
             else:
-                print(f'Applying modifier {func.__name__}')
+                self.logger.info(f'Applying modifier {func.__name__}')
                 data = func(data, **kwargs)
 
         feature_names = [f[0] for f in self.transformer_funcs if f[0]]
         data = self.combine_dataframes([data[k] for k in feature_names])
         print('Pipeline finished')
-        return data
+        return data.sort_index()
 
     @staticmethod
     def combine_dataframes(list_of_dataframes):
