c3972b09c16a5d477bfa1c3d33c190425e5a5053
Javier Mas <javier.mas@glovoapp.com>
2020-10-04 15:35:40 +0200

    :building_construction: Develop setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import io
+import os
+from setuptools import find_packages, setup
 
 import soccerlearn
 
-SHORT = 'soccerlearn'
+# Meta-data
+NAME = 'soccerlearn'
+DESCRIPTION = 'Repository for soccerlearn'
+HERE = os.path.abspath(os.path.dirname(__file__))
+REQUIRES_PYTHON = '>=3.8.0, <3.9.0'
+try:
+    with io.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+        LONG_DESCRIPTION = '\n' + f.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+def list_requirements(fname='requirements.txt'):
+    with open(fname) as fd:
+        return fd.read().splitlines()
 
 setup(
-    name='soccerlearn',
+    name=NAME,
     version=soccerlearn.__version__,
     packages=[
         'soccerlearn'
@@ -17,8 +29,10 @@ setup(
     classifiers=(
         'Programming Language :: Python :: 3.6'
     ),
-    description=SHORT,
-    long_description=SHORT,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    install_requires=list_requirements(),
+    requires_python=REQUIRES_PYTHON,
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
 )
