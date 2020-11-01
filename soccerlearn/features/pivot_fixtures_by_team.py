b6f94b9dbb097d1cfee7742417202c3cc2108843
Javier Mas <javier.mas@glovoapp.com>
2020-11-01 11:58:03 +0100

    :sparkles: Add opposite_team_id and h2h_id to pivot_fixtures_by_team

--- soccerlearn/features/pivot_fixtures_by_team.py
+++ soccerlearn/features/pivot_fixtures_by_team.py
@@ -5,7 +5,7 @@ def pivot_fixtures_by_team(data):
     fixtures = data['fixtures']
     odd_vars = [col for col in fixtures if 'odd' in col]
     cols_to_keep = ['round_id', 'date', 'scores', 'winner_team_id',
-                    'season_id', 'season_start_year']
+                    'season_id', 'season_start_year', 'h2h_id']
     features = pd.melt(
         fixtures,
         id_vars=cols_to_keep+ odd_vars,
@@ -13,5 +13,9 @@ def pivot_fixtures_by_team(data):
         value_name='team_id'
     )
     features['local'] = features['variable'].apply(lambda x: x == 'localteam_id')
+    features['opposite_team_id'] = features.apply(
+        lambda x: int([v for v in x['h2h_id'].split('_') if v != str(x['team_id'])][0]), axis=1
+    )
     features = features.drop(columns='variable')
-    return features.set_index(['team_id', 'date'])
+    data['fixtures'] = features.set_index(['team_id', 'date', 'season_id'])
+    return data
