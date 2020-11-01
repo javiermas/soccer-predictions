f3e63bec4d873fa86257980017e9d52b9cac0411
Javier Mas <javier.mas@glovoapp.com>
2020-11-01 12:07:41 +0100

    :bug: Remove ptvsd

--- soccerlearn/features/home_and_visitor_features.py
+++ soccerlearn/features/home_and_visitor_features.py
@@ -2,13 +2,12 @@ import pandas as pd
 
 
 COLUMNS_NO_TRANSFORM = ['date', 'result', 'team_id', 'season_id', 'local_team_id', 'visitor_team_id',
-                             'h2h_games_played_sum', 'h2h_draw_mean']
+                        'h2h_games_played_sum', 'h2h_draw_mean']
 COLUMNS_NO_DIFFERENCE = COLUMNS_NO_TRANSFORM + ['name', 'founded', 'short_code']
 
 
 def compute_home_and_visitor_features(data):
     features = data['features'].copy()
-    import ptvsd; ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True);ptvsd.wait_for_attach()
     original_columns = list(features.columns)
     features = _add_local_team_id(features, data['fixtures'])
     features = _add_visitor_team_id(features, data['fixtures'])
@@ -45,9 +44,8 @@ def compute_home_and_visitor_features(data):
 
 def add_difference_features(df, feature_names):
     for name in feature_names:
-        print(name)
         df[f'difference_in_{name}'] = df[f'local_{name}'] - df[f'visitor_{name}']
-        
+
     return df
 
 
@@ -73,6 +71,7 @@ def _get_local_team_id(fixtures):
         'local_team_id': local_team_id.tolist(),
     }, index=fixtures.index)
     return local_team_id
+
 
 def _get_visitor_team_id(fixtures):
     visitor_team_id = fixtures.reset_index().apply(lambda x: x['team_id'] if not x['local'] else x['opposite_team_id'], axis=1)
