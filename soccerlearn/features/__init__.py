053e12d9aa2323b704eb1a0a4a42c16c0194d0dc
Javier Mas <javier.mas@glovoapp.com>
2020-11-01 12:01:45 +0100

    :building_construction: Update init of features

--- soccerlearn/features/__init__.py
+++ soccerlearn/features/__init__.py
@@ -2,4 +2,11 @@ from .unpivot_fixtures_by_odds import unpivot_fixtures_by_odds
 from .pivot_fixtures_by_team import pivot_fixtures_by_team
 from .team_date_features import *
 from .team_season_features import *
+from .preprocessors import *
 from .pipeline import Pipeline
+from .home_and_visitor_features import *
+
+
+#import pandas as pd
+#pd.set_option('display.max_columns', 20)
+#pd.set_option('display.width', 300)
